580604d85a4e27142b0a9f532d18073a896946e9
Nathan Arnold <natearn@natearn.info>
2012-05-08 14:56:07 -0400

    added a TODO list

--- streamsync.sh
+++ streamsync.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+#TODO:
+#	1. support file paths for $1
+#	2. replace .* with appropriate part-file extensions
+#	3. support arbitrary rsync options
+#	4. use more efficient method of repeating the rsync command
+
 while [ -e "$1".* ]; do
 	rsync --inplace --progress "$1".* "$2"/"$1"
 done
