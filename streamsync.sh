25ab7bef93e0a48038b33f198b7ca88eac1378ff
Nathan Arnold <natearn@natearn.info>
2012-05-26 10:44:27 -0400

    added sleep so that it doesn't spam

--- streamsync.sh
+++ streamsync.sh
@@ -4,7 +4,12 @@
 #	1. support file paths and names in all arguments
 #	2. replace .* with a more specific list of extensions
 
-while [ -e "$1" ]; do
-	rsync --inplace --progress "$1" "$2"/"$1"
+while [ -e "$1.*" ]; do
+	rsync --inplace --progress "$1.*" "$2"/`basename "$1"`
+	if [ -z $3 ]; then
+		sleep $3
+	else
+		sleep 10
+	fi
 done
-rsync --inplace --progress "$1" "$2"/"$1"
+rsync --inplace --progress "$1" "$2"/`basename "$1"`
