cfb24a1b2d882c507f31ba65f140d94f10cd40ad
Nathan Arnold <natearn@natearn.info>
2012-05-26 15:49:45 -0400

    removed third argument

--- streamsync.sh
+++ streamsync.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-#TODO:
-#	1. support file paths and names in all arguments
-#	2. replace .* with a more specific list of extensions
+#TODO: replace .* with a more specific list of extensions
 
-while [ -e "$1.*" ]; do
-	rsync --inplace --progress "$1.*" "$2"/`basename "$1"`
-	if [ -z $3 ]; then
-		sleep $3
-	else
-		sleep 10
-	fi
+while [ -e "$1".* ]; do
+	rsync --inplace --progress "$1".* "$2"/`basename "$1"`
+	sleep 10
 done
 rsync --inplace --progress "$1" "$2"/`basename "$1"`
